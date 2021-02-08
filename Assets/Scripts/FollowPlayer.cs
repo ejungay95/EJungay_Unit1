6a4ae21319e410fcaa2a3d33a1f83dbbf7b03d3e
ejungay95 <elijah.jungay@gmail.com>
2021-02-08 15:16:10 -0800

    Finished Unit1: Lesson C

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class FollowPlayer : MonoBehaviour {
 
-  //public float speed = 20.0f;
-  public int speed = 20;
+  public GameObject player;
+
+  private Vector3 offset = new Vector3(0, 5, -6);
 
   // Start is called before the first frame update
   void Start() {
-        
+    
   }
 
   // Update is called once per frame
   void Update() {
-    // Move the car forward
-    transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    transform.position = player.transform.position + offset;
   }
 }
